4187d436a7385c55f66ddd825ef56f7c95cfd891
TaiwanCCyoyo <xjp0134323@gmail.com>
2020-08-22 02:55:29 +0800

    modify for no fence

--- v/vm.c
+++ v/vm.c
@@ -17,14 +17,16 @@
 void trap_entry();
 void pop_tf(trapframe_t*);
 
-extern volatile uint64_t tohost;
-extern volatile uint64_t fromhost;
+volatile uint32_t *my_tohost = 0xC1000000;
+volatile uint64_t tohost;
+volatile uint64_t fromhost;
 
 static void do_tohost(uint64_t tohost_value)
 {
   while (tohost)
     fromhost = 0;
   tohost = tohost_value;
+ *my_tohost = tohost_value;
 }
 
 #define pa2kva(pa) ((void*)(pa) - DRAM_BASE - MEGAPAGE_SIZE)
@@ -62,10 +64,9 @@ void wtf()
 
 #define stringify1(x) #x
 #define stringify(x) stringify1(x)
-#define assert(x) do { \
+#define assert(x , y) do { \
   if (x) break; \
-  cputstring("Assertion failed: " stringify(x) "\n"); \
-  terminate(3); \
+  terminate(y); \
 } while(0)
 
 #define l1pt pt[0]
@@ -109,17 +110,17 @@ void printhex(uint64_t x)
 
 static void evict(unsigned long addr)
 {
-  assert(addr >= PGSIZE && addr < MAX_TEST_PAGES * PGSIZE);
+  assert(addr >= PGSIZE && addr < MAX_TEST_PAGES * PGSIZE, 123);
   addr = addr/PGSIZE*PGSIZE;
 
   freelist_t* node = &user_mapping[addr/PGSIZE];
   if (node->addr)
   {
     // check accessed and dirty bits
-    assert(user_llpt[addr/PGSIZE] & PTE_A);
+    assert(user_llpt[addr/PGSIZE] & PTE_A, 1);
     uintptr_t sstatus = set_csr(sstatus, SSTATUS_SUM);
     if (memcmp((void*)addr, uva2kva(addr), PGSIZE)) {
-      assert(user_llpt[addr/PGSIZE] & PTE_D);
+      assert(user_llpt[addr/PGSIZE] & PTE_D, 2);
       memcpy((void*)addr, uva2kva(addr), PGSIZE);
     }
     write_csr(sstatus, sstatus);
@@ -138,14 +139,14 @@ static void evict(unsigned long addr)
 
 void handle_fault(uintptr_t addr, uintptr_t cause)
 {
-  assert(addr >= PGSIZE && addr < MAX_TEST_PAGES * PGSIZE);
+  assert(addr >= PGSIZE && addr < MAX_TEST_PAGES * PGSIZE, 100);
   addr = addr/PGSIZE*PGSIZE;
 
   if (user_llpt[addr/PGSIZE]) {
     if (!(user_llpt[addr/PGSIZE] & PTE_A)) {
       user_llpt[addr/PGSIZE] |= PTE_A;
     } else {
-      assert(!(user_llpt[addr/PGSIZE] & PTE_D) && cause == CAUSE_STORE_PAGE_FAULT);
+      assert(!(user_llpt[addr/PGSIZE] & PTE_D) && cause == CAUSE_STORE_PAGE_FAULT, 4);
       user_llpt[addr/PGSIZE] |= PTE_D;
     }
     flush_page(addr);
@@ -153,7 +154,7 @@ void handle_fault(uintptr_t addr, uintptr_t cause)
   }
 
   freelist_t* node = freelist_head;
-  assert(node);
+  assert(node, 5);
   freelist_head = node->next;
   if (freelist_head == freelist_tail)
     freelist_tail = 0;
@@ -162,7 +163,7 @@ void handle_fault(uintptr_t addr, uintptr_t cause)
   user_llpt[addr/PGSIZE] = new_pte | PTE_A | PTE_D;
   flush_page(addr);
 
-  assert(user_mapping[addr/PGSIZE].addr == 0);
+  assert(user_mapping[addr/PGSIZE].addr == 0, 6);
   user_mapping[addr/PGSIZE] = *node;
 
   uintptr_t sstatus = set_csr(sstatus, SSTATUS_SUM);
@@ -181,14 +182,14 @@ void handle_trap(trapframe_t* tf)
   {
     int n = tf->gpr[10];
 
-    for (long i = 1; i < MAX_TEST_PAGES; i++)
+    for (long i = 1; i < 2; i++)
       evict(i*PGSIZE);
 
     terminate(n);
   }
   else if (tf->cause == CAUSE_ILLEGAL_INSTRUCTION)
   {
-    assert(tf->epc % 4 == 0);
+    assert(tf->epc % 4 == 0, 7);
 
     int* fssr;
     asm ("jal %0, 1f; fssr x0; 1:" : "=r"(fssr));
@@ -196,13 +197,13 @@ void handle_trap(trapframe_t* tf)
     if (*(int*)tf->epc == *fssr)
       terminate(1); // FP test on non-FP hardware.  "succeed."
     else
-      assert(!"illegal instruction");
+      assert(!"illegal instruction", 8);
     tf->epc += 4;
   }
   else if (tf->cause == CAUSE_FETCH_PAGE_FAULT || tf->cause == CAUSE_LOAD_PAGE_FAULT || tf->cause == CAUSE_STORE_PAGE_FAULT)
     handle_fault(tf->badvaddr, tf->cause);
   else
-    assert(!"unexpected exception");
+    assert(!"unexpected exception", 9);
 
   pop_tf(tf);
 }
@@ -257,7 +258,7 @@ void vm_boot(uintptr_t test_addr)
                         | (vm_choice * (SATP_MODE & ~(SATP_MODE<<1)));
   write_csr(sptbr, sptbr_value);
   if (read_csr(sptbr) != sptbr_value)
-    assert(!"unsupported satp mode");
+    assert(!"unsupported satp mode", 10);
 
   // Set up PMPs if present, ignoring illegal instruction trap if not.
   uintptr_t pmpc = PMP_NAPOT | PMP_R | PMP_W | PMP_X;
@@ -267,7 +268,7 @@ void vm_boot(uintptr_t test_addr)
                 "csrw pmpaddr0, %1\n\t"
                 "csrw pmpcfg0, %0\n\t"
                 ".align 2\n\t"
-                "1: csrw mtvec, t0"
+                "1:"
                 : : "r" (pmpc), "r" (pmpa) : "t0");
 
   // set up supervisor trap handling
@@ -278,8 +279,8 @@ void vm_boot(uintptr_t test_addr)
     (1 << CAUSE_FETCH_PAGE_FAULT) |
     (1 << CAUSE_LOAD_PAGE_FAULT) |
     (1 << CAUSE_STORE_PAGE_FAULT));
-  // FPU on; accelerator on; vector unit on
-  write_csr(mstatus, MSTATUS_FS | MSTATUS_XS | MSTATUS_VS);
+  // FPU on; accelerator on; allow supervisor access to user memory access
+  write_csr(mstatus, MSTATUS_FS | MSTATUS_XS);
   write_csr(mie, 0);
 
   random = 1 + (random % MAX_TEST_PAGES);
@@ -293,8 +294,13 @@ void vm_boot(uintptr_t test_addr)
   }
   freelist_nodes[MAX_TEST_PAGES-1].next = 0;
 
+  //for uart
+  l1pt[((((unsigned int)my_tohost)>>PTE_PPN_SHIFT)>>RISCV_PGSHIFT)] = (((((unsigned int)my_tohost))/RISCV_PGSIZE)<< PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_A | PTE_D;
+
   trapframe_t tf;
   memset(&tf, 0, sizeof(tf));
   tf.epc = test_addr - DRAM_BASE;
+  int user_l2pt_entry = ((unsigned int)test_addr) << 10 >> 22;
+  // user_llpt[user_l2pt_entry] = ((((unsigned int)test_addr)/RISCV_PGSIZE) << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X | PTE_A | PTE_D | PTE_U;
   pop_tf(&tf);
 }
